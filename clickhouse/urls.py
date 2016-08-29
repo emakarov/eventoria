34f97c630a6dd97d3931ae23d2eaca2b6ddee9f6
Evgeni Makarov <evgeni.makarov@gmail.com>
2016-08-30 01:02:31 +0300

    using format JSON in all queries. Initial dashboard html template

--- clickhouse/urls.py
+++ clickhouse/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls import url
-from views import api
+from views import api, dashboard
 
 urlpatterns = [
-  url(r'api/$', api)
+  url(r'api/$', api),
+  url(r'dashboard/$', dashboard)
 ]
--- clickhouse/views.py
+++ clickhouse/views.py
@@ -6,26 +6,24 @@ import urllib2
 import requests
 import csv
 import json
+from django.template import Template, RequestContext, loader, Context
 
-# Create your views here.
 
 CLICKHOUSE_BASE_URL = 'http://localhost:8123/?query=' 
+CLICKHOUSE_DASHBOARD_TEMPLATE = 'clickhouse/dashboard.html'
 
 def api(request):
     query = request.GET['query']
     database = request.GET.get('database', None)
     table = request.GET.get('table', None)
+    query += " format JSON"
     url = CLICKHOUSE_BASE_URL + query
     if database is not None:
         url += '&database=' + database
     data = requests.get(url)	
-    if table is None:
-        objects = [ [y for y in x.split('\t')] for x in data.text.splitlines()]
-    else:
-        urld = CLICKHOUSE_BASE_URL + 'describe table ' + table
-        describe = requests.get(urld)
-        describer = [ [y for y in x.split('\t')] for x in describe.text.splitlines()]
-        objects = [ {describer[i][0]:y for i,y in enumerate(x.split('\t'))} for x in data.text.splitlines()]
-                
-    json_data = json.dumps({'objects': objects})
+    json_data = data.text
     return HttpResponse(json_data, content_type='application/json')
+
+
+def dashboard(request):
+    return render(request, CLICKHOUSE_DASHBOARD_TEMPLATE, {})
